d7b6fcbff9eec969430b96009674ac23cb1c8b2f
darah3ng <daragitgud@gmail.com>
2022-02-21 12:23:00 +1000

    chore: update FE

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { useEffect, useMemo, useState } from 'react';
 import { Container, Heading, Box, Button } from '@chakra-ui/react';
 import { useWeb3 } from '@3rdweb/hooks';
 
+const headingBgGradient = 'linear(to-r, #1ab3b3, #20dfdf, #dfaf20)';
+const buttonBgGradient = 'linear(to-r, #062d2d, #0d5959, #138686)';
+
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
-  console.log("👋 Address:", address);
 
   function renderNonConnectedWallet() {
     return (
       <>
-        <Heading>Welcome to ComedyDAO</Heading>
-        <Button onClick={() => connectWallet('injected')}>
+        <Heading bgGradient={headingBgGradient} bgClip={'text'}>Welcome to ComedyDAO</Heading>
+        <Button
+          mt={5}
+          bgGradient={buttonBgGradient}
+          _hover={{ bg: '#20dfdf' }}
+          color={'white'} 
+          onClick={() => connectWallet('injected')}
+        >
           Connect your wallet
         </Button>
       </>
@@ -19,12 +27,14 @@ const App = () => {
 
   function renderConnectedWallet() {
     return (
-      <Heading>Wallet connected, let's dance! 💃</Heading>
+      <>
+        <Heading bgGradient={headingBgGradient} bgClip={'text'}>Wallet connected, let's dance! 💃</Heading>
+      </>
     )
   }
 
   return (
-    <Container mt={10}>
+    <Container mt={10} centerContent>
       {!address ? renderNonConnectedWallet() : renderConnectedWallet()}
     </Container>
   )
